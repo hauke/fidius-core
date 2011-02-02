571501ac44911f16873c3b2db416ccb92668c118
dkohl <dkohl@wlan-client-121.informatik.uni-bremen.de>
2011-02-02 14:07:11 +0100

    Del

--- lib/fidius/agent/predictor.rb
+++ lib/fidius/agent/predictor.rb
@@ -1,12 +1,38 @@
-require 'rubygems'
-require 'ai4r'
-
-include Ai4r
+module FIDIUS
 
 module MachineLearning
+  include Ai4r
 
   NODES_PER_LAYER = 10
   OUT = 1
+  
+  def known_services
+    services = []    
+    services << Service.new("ssh", 22, "tcp")
+    services << Service.new("vnc", 5900, "tcp")
+    services << Service.new("smtp", 25, "tcp")
+    services << Service.new("domain", 53, "udp")
+    services << Service.new("mysql", 3306, "tcp")
+    services << Service.new("http", 80, "tcp")
+    services << Service.new("netbios-ssn", 139, "tcp")
+    services << Service.new("netbios-ssn", 139, "udp")
+    services << Service.new("microsoft-ds", 445, "tcp")
+    services << Service.new("ftp", 21, "tcp")
+    services << Service.new("ipp", 631, "tcp")
+    services << Service.new("ipp", 631, "udp")
+    services << Service.new("afp", 548, "tcp")
+    services << Service.new("afp", 548, "udp")
+    services << Service.new("kerberos-sec", 88, "tcp")
+    services << Service.new("kerberos-sec", 88, "udp")
+    services << Service.new("https", 443, "tcp")
+    services << Service.new("svn", 3690, "tcp")
+    services << Service.new("aol", 5190, "tcp")
+    services << Service.new("http-proxy", 1080, "tcp")
+    services << Service.new("pop3", 110, "tcp")
+    services << Service.new("ldap", 389, "tcp")
+    services << Service.new("cvspserver", 2401, "tcp")
+    services << Service.new("imap", 143, "tcp")
+  end
 
   class Predictor
 
@@ -47,7 +73,7 @@ module MachineLearning
     end
   end
 
-  def str2example line
+  def str2example lin
     example = []
     str = line.split(",")
     str.each do |s|
@@ -57,29 +83,4 @@ module MachineLearning
   end
 end
 
-# TEST
-include MachineLearning
-
-set = []
-labels = []
-
-fp = File.open("nmap.csv", "r")
-while line = fp.gets
-  set << str2example(line)
-end
-fp.close
-
-fp = File.open("pred.csv", "r")
-while line = fp.gets
-   labels << [line.to_i]
-end
-fp.close
-
-pred =  Predictor.new(set, labels, 2)
-300.times do |i|
-  pred.train
-end
-
-set.size.times do |x|
-  puts "#{pred.predict(set[x])}" # "#{labels[x]}"
 end
