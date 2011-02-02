3f120fed7446614f693944eb40c53658ef4ba305
Dominik Menke <dmke@tzi.de>
2011-02-02 17:06:17 +0100

    hm?

--- lib/fidius/knowledge/service.rb
+++ lib/fidius/knowledge/service.rb
@@ -10,5 +10,9 @@ class Service < Struct.new(:name, :port, :proto)
     raise NotImplementedError, "not implemented yet"
   end
 
+  def == x
+    @name.eql? x.name and @port == x.port and @proto == x.proto      
+  end
+
 end # class Service
 end # module FIDIUS
