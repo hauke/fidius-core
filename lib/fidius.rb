f451e5577cca3313fa07ff7fc7c9e4f1098bf176
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2011-02-02 16:36:41 +0100

    commit to be able to pull

--- lib/fidius.rb
+++ lib/fidius.rb
@@ -1,20 +1,25 @@
 module FIDIUS
+  
   # MISC
   require 'rubygems'
 
   # KNOWLEDGE
-  require File.join(File.dirname(__FILE__),'fidius' ,'knowledge', 'service')
-  require File.join(File.dirname(__FILE__),'fidius' , 'knowledge', 'loudness')
-  require File.join(File.dirname(__FILE__),'fidius' , 'knowledge', 'subnet')
-  require File.join(File.dirname(__FILE__),'fidius' , 'knowledge', 'asset')
+  require File.join(File.dirname(__FILE__),'fidius', 'knowledge', 'service')
+  require File.join(File.dirname(__FILE__),'fidius', 'knowledge', 'loudness')
+  require File.join(File.dirname(__FILE__),'fidius', 'knowledge', 'subnet')
+  require File.join(File.dirname(__FILE__),'fidius', 'knowledge', 'asset')
 
   # AI
   require 'ai4r'
   require 'algorithms'  
-  require File.join(File.dirname(__FILE__),'fidius' , 'agent', 'agent')
-  require File.join(File.dirname(__FILE__),'fidius' , 'agent', 'predictor')
+  require File.join(File.dirname(__FILE__),'fidius', 'agent', 'agent')
+  require File.join(File.dirname(__FILE__),'fidius', 'agent', 'predictor')
 
   # ACTION
   require File.join(File.dirname(__FILE__),'fidius' , 'action', 'scan')
   
+  
+
 end
+
+
--- test/fidius/test_host.rb
+++ test/fidius/test_host.rb
@@ -1,3 +1,5 @@
+
+
 require File.join(File.dirname(__FILE__), '..', '..','lib', 'fidius')
 
 def test_host
