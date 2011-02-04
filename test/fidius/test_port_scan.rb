9d16edc1d99e9ef59fb9305cfabdbb72d249ec60
dkohl <dkohl@wlan-client-102.informatik.uni-bremen.de>
2011-02-04 13:07:13 +0100

    Services

--- test/fidius/test_port_scan.rb
+++ test/fidius/test_port_scan.rb
@@ -3,3 +3,5 @@ require File.join(File.dirname(__FILE__), '..', '..','lib', 'fidius')
 h = FIDIUS::Asset::Host.new("marie", "134.102.201.101")
 scan = FIDIUS::Action::Scan::PortScan.new(h)
 target = scan.execute
+
+p target.get_services_as_bit_vector 
