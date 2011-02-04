96ab5e2f2b278b6666ebd3d99e317e57ddec2cad
Christian Arndt <arndtc@tzi.de>
2011-02-04 12:45:50 +0100

    add misc from msf/lib

--- lib/fidius.rb
+++ lib/fidius.rb
@@ -2,6 +2,8 @@ module FIDIUS
   
   # MISC
   require 'rubygems'
+  require 'misc/nmap_xml' # copied from msf/lib
+  require 'misc/file'     # copied from msf/lib
   
   $LOAD_PATH.unshift File.expand_path(File.dirname __FILE__)
 
@@ -15,13 +17,15 @@ module FIDIUS
   require 'fidius/action/scan'
   require 'fidius/action/scan/port_scan'
   require 'rex'
+  require 'tempfile'
+  require "fileutils"
 
   # AI
   require 'ai4r'
   require 'algorithms'
   require 'fidius/agent/agent'
   require 'fidius/agent/predictor'
-
+  
 end
 
 
