85ecd7b4bc554b840ce86022b490339febc7c641
Christian Arndt <arndtc@tzi.de>
2011-02-04 13:09:59 +0100

    working port_scan -- whooooohoo

--- lib/fidius/action/scan/port_scan.rb
+++ lib/fidius/action/scan/port_scan.rb
@@ -12,6 +12,7 @@ module FIDIUS
         def initialize target, port_range = nil
           raise ArgumentError, "target isnt a target-Object" unless target.ip
           @target = target
+          @target.services = []
           @port_range = port_range
         end
         
@@ -25,12 +26,12 @@ module FIDIUS
             args = [" -sV ", @target.ip]
           end
           nmap = Rex::FileUtils.find_full_path("nmap") || Rex::FileUtils.find_full_path("nmap.exe")
-          
+
           if (not nmap)
             puts("The nmap executable could not be found")
             return
           end
-
+          
           fd = Tempfile.new('xmlnmap')
           fd.binmode
           args.push('-oX', fd.path)
@@ -40,17 +41,16 @@ module FIDIUS
             data = f.read(f.stat.size)
           end
           arguments = {:filename => fd.path}.merge(:data => data)
-          # Use a stream parser instead of a tree parser so we can deal with                                                                                                    
-          # huge results files without running out of memory.                                                                                                                   
+          # Use a stream parser instead of a tree parser so we can deal with
+          # huge results files without running out of memory.
           parser = Rex::Parser::NmapXMLStreamParser.new
-          parser.on_found_host = Proc.new { |h|
+          parser.on_found_host = Proc.new do |h|
             h["ports"].each do |p|
               if p["state"] == "open"
                 @target.services << FIDIUS::Service.new(p["name"], p["portid"].to_i, p["protocol"])
               end
             end
           end
-          f.close
           fd.close(true)
           REXML::Document.parse_stream(data, parser)
           return @target
