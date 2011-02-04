3b3b108c2b94d8e5665547aeae82a3e4268d93fb
dkohl <dkohl@wlan-client-102.informatik.uni-bremen.de>
2011-02-04 13:25:12 +0100

    Extend Agent test

--- test/fidius/test_agent.rb
+++ test/fidius/test_agent.rb
@@ -1,15 +1,15 @@
 require File.join(File.dirname(__FILE__), '..', '..','lib', 'fidius')
 include FIDIUS::MachineLearning
 
-h1 = FIDIUS::Asset::Host.new("KEEEEEKS")
+# Training set
+h1 = FIDIUS::Asset::Host.new("KEEEEEKS", "")
 h1.services = []
 h1.services << FIDIUS::Service.new("ssh", 22, "tcp")
 h1.services << FIDIUS::Service.new("vnc", 5900, "tcp")
 h1.services << FIDIUS::Service.new("smtp", 25, "tcp")
 h1.services << FIDIUS::Service.new("domain", 53, "udp")
 inst = Instance.new(h1, 2)
-
-h2 = FIDIUS::Asset::Host.new("KEEEEEKS2")
+h2 = FIDIUS::Asset::Host.new("KEEEEEKS2", "")
 h2.services = []
 h2.services << FIDIUS::Service.new("ssh", 22, "tcp")
 h2.services << FIDIUS::Service.new("vnc", 5900, "tcp")
@@ -17,5 +17,15 @@ h2.services << FIDIUS::Service.new("smtp", 25, "tcp")
 h2.services << FIDIUS::Service.new("domain", 53, "udp")
 inst2 = Instance.new(h2, 3)
 
+# Test set
+h3 = FIDIUS::Asset::Host.new("101", "134.102.201.101")
+h4 = FIDIUS::Asset::Host.new("102", "134.102.201.102")
+h5 = FIDIUS::Asset::Host.new("103", "134.102.201.103")
+
+# Train agent
 agent = Agent.new
 agent.train([inst, inst2], 100)
+
+# Choose next
+dec = agent.decision([h3,h4,h5])
+puts "hack #{dec.name} with ip #{dec.ip} next"
