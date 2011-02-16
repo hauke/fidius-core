395558c4173d91997aa42c008e329363f0b82753
Dominik Menke <dmke@tzi.de>
2011-02-16 20:07:01 +0100

    expanded class/module names (and re-indented) in FIDIUS::Action::Scan

--- lib/fidius/action/scan/ping_scan.rb
+++ lib/fidius/action/scan/ping_scan.rb
@@ -1,44 +1,50 @@
-class FIDIUS::Action::Scan::PingScan
+module FIDIUS
+  module Action
+    module Scan
+      class PingScan
 
-  include FIDIUS::Action::Scan
+        include FIDIUS::Action::Scan
 
-  # returns an array of hosts (which are up) in a given subnet
+        # returns an array of hosts (which are up) in a given subnet
 
-  def initialize subnet
-    raise ArgumentError, "target not set" unless subnet
-    @subnet = subnet
-    @hosts = []
-  end
+        def initialize subnet
+          raise ArgumentError, "target not set" unless subnet
+          @subnet = subnet
+          @hosts = []
+        end
 
-  def execute
-    args = ["-sP", @subnet]
-    nmap = Rex::FileUtils.find_full_path("nmap") || Rex::FileUtils.find_full_path("nmap.exe")
-    if (not nmap)
-      puts("The nmap executable could not be found")
-      return
-    end
+        def execute
+          args = ["-sP", @subnet]
+          nmap = Rex::FileUtils.find_full_path("nmap") || Rex::FileUtils.find_full_path("nmap.exe")
+          if (not nmap)
+            puts("The nmap executable could not be found")
+            return
+          end
 
-    fd = Tempfile.new('xmlnmap')
-    fd.binmode
-    args.push('-oX', fd.path)
-    raise "Nmap ping-scan failed" unless (system("#{nmap} #{args.join(' ')}"))
-    data = ""
-    ::File.open(fd.path, 'rb') do |f|
-      data = f.read(f.stat.size)
-    end
-    arguments = {:filename => fd.path}.merge(:data => data)
+          fd = Tempfile.new('xmlnmap')
+          fd.binmode
+          args.push('-oX', fd.path)
+          raise "Nmap ping-scan failed" unless (system("#{nmap} #{args.join(' ')}"))
+          data = ""
+          ::File.open(fd.path, 'rb') do |f|
+            data = f.read(f.stat.size)
+          end
+          arguments = {:filename => fd.path}.merge(:data => data)
 
-    parser = Rex::Parser::NmapXMLStreamParser.new
-    parser.on_found_host = Proc.new do |h|
-      if h["status"] == "up"
-        puts h["addrs"]["ipv4"]
-        @hosts << h["addrs"]["ipv4"]
-      end
-    end
-    fd.close(true)
-    REXML::Document.parse_stream(data, parser)
-    return @hosts
-  end
+          parser = Rex::Parser::NmapXMLStreamParser.new
+          parser.on_found_host = Proc.new do |h|
+            if h["status"] == "up"
+              puts h["addrs"]["ipv4"]
+              @hosts << h["addrs"]["ipv4"]
+            end
+          end
+          fd.close(true)
+          REXML::Document.parse_stream(data, parser)
+          return @hosts
+        end
 
-end
+      end # class PingScan
+    end # module Scan
+  end # module Action
+end # module FIDIUS
 
--- lib/fidius/action/scan/port_scan.rb
+++ lib/fidius/action/scan/port_scan.rb
@@ -1,50 +1,57 @@
-class FIDIUS::Action::Scan::PortScan
-  include FIDIUS::Action::Scan
+module FIDIUS
+  module Action
+    module Scan
+      class PortScan
+        include FIDIUS::Action::Scan
 
-  def initialize target, port_range = nil
-    raise ArgumentError, "target isnt a target-Object" unless target.ip
-    @target = target
-    @target.services = []
-    @port_range = port_range
-  end
-  
-  def execute 
-    
-    raise ArgumentError, "target not set" unless @target
-    
-    if @port_range
-      args = ["-sV -p #{@port_range}", @target.ip]
-    else
-      args = ["-sV", @target.ip]
-    end
-    nmap = Rex::FileUtils.find_full_path("nmap") || Rex::FileUtils.find_full_path("nmap.exe")
-
-    if (not nmap)
-      puts("The nmap executable could not be found")
-      return
-    end
-    
-    fd = Tempfile.new('xmlnmap')
-    fd.binmode
-    args.push('-oX', fd.path)
-    data = ""
-    ::File.open(fd.path, 'rb') do |f|
-      data = f.read(f.stat.size)
-    end
-    arguments = {:filename => fd.path}.merge(:data => data)
-    # Use a stream parser instead of a tree parser so we can deal with
-    # huge results files without running out of memory.
-    parser = Rex::Parser::NmapXMLStreamParser.new
-    parser.on_found_host = Proc.new do |h|
-      h["ports"].each do |p|
-        if p["state"] == "open"
-          @target.services << FIDIUS::Service.new(p["name"], p["portid"].to_i, p["protocol"])
+        def initialize target, port_range = nil
+          raise ArgumentError, "target isnt a target-Object" unless target.ip
+          @target = target
+          @target.services = []
+          @port_range = port_range
         end
-      end
-    end
-    fd.close(true)
-    REXML::Document.parse_stream(data, parser)
-    return @target
-  end
-end
+        
+        def execute 
+          
+          raise ArgumentError, "target not set" unless @target
+          
+          if @port_range
+            args = ["-sV -p #{@port_range}", @target.ip]
+          else
+            args = ["-sV", @target.ip]
+          end
+          nmap = Rex::FileUtils.find_full_path("nmap") || Rex::FileUtils.find_full_path("nmap.exe")
+
+          if (not nmap)
+            puts("The nmap executable could not be found")
+            return
+          end
+          
+          fd = Tempfile.new('xmlnmap')
+          fd.binmode
+          args.push('-oX', fd.path)
+          data = ""
+          ::File.open(fd.path, 'rb') do |f|
+            data = f.read(f.stat.size)
+          end
+          arguments = {:filename => fd.path}.merge(:data => data)
+          # Use a stream parser instead of a tree parser so we can deal with
+          # huge results files without running out of memory.
+          parser = Rex::Parser::NmapXMLStreamParser.new
+          parser.on_found_host = Proc.new do |h|
+            h["ports"].each do |p|
+              if p["state"] == "open"
+                @target.services << FIDIUS::Service.new(p["name"], p["portid"].to_i, p["protocol"])
+              end
+            end
+          end
+          fd.close(true)
+          REXML::Document.parse_stream(data, parser)
+          return @target
+        end
+        
+      end # class PortScan
+    end # module Scan
+  end # module Action
+end # module FIDIUS
 
