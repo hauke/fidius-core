0366dcc44899a2864efb0fe2912a8be94b1c6517
bernd <B.Katzmarski@gmx.de>
2011-02-16 17:53:59 +0100

    xml_rpc

--- bin/xml_server.rb
+++ bin/xml_server.rb
@@ -3,31 +3,29 @@ require "xmlrpc/server"
 
 s = XMLRPC::Server.new(8080,"127.0.0.1") 
 
-s.add_handler("auth.login") do |user,pwd|
-  "asa"
-end
-
 s.add_handler("model.host.find") do |opts|
-  #FIDIUS::Asset::Host.destroy_all
-  erg = FIDIUS::Asset::Host.create(:name => 50)
-  #erg.name = 40
-  #erg.save
-  puts erg.inspect
-  #erg = FIDIUS::Asset::Host.first
-  erg.to_xml
-end
-
-s.add_handler("michael.div") do |a,b|
-  if b == 0
-    raise XMLRPC::FaultException.new(1, "division by zero")
-  else
-    a / b 
+  # TODO: find :all
+  # TODO: more generic, not only hosts
+  res = nil
+  
+  if opts.size == 1
+    id = opts[0].to_i
+    if !FIDIUS::Asset::Host.exists?(id)
+      puts "raise"
+      raise XMLRPC::FaultException.new(1, "object does not exist")
+    end
+    res = FIDIUS::Asset::Host.find(id)
+  end 
+  puts "i am here and res is: #{res}"
+  unless res
+    puts "throw error"
+    raise XMLRPC::FaultException.new(1, "object was not found")
   end
-end 
+  res.to_xml
+end
 
 s.set_default_handler do |name, *args|
   raise XMLRPC::FaultException.new(-99, "Method #{name} missing" +
                                    " or wrong number of parameters!")
 end
-puts "HELLO"
 s.serve
