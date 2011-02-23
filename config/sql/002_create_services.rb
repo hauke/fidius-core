e3d29cda4cba1f315d73355cce825ff50fb1ba4d
Dominik Menke <dmke@tzi.de>
2011-02-23 15:53:27 +0100

    fixed some implementations to gain less failing tests

---
+++ config/sql/002_create_services.rb
@@ -0,0 +1,14 @@
+class CreateServices < ActiveRecord::Migration
+	def self.up
+		create_table :services do |t|
+			t.string :name
+			t.string :port
+			t.string :proto
+			t.integer :host_id
+		end
+	end
+	
+	def self.down
+		drop_table :services
+	end
+end
