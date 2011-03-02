5b2f9b15cce90a986ef35e5bfc38b28702d540ea
Dominik Menke <dmke@tzi.de>
2011-03-02 18:40:13 +0100

    MsfDRbd++, replaced old RPC connection in FIDIUS::Action::Exploit::Exploit

--- lib/fidius/action.rb
+++ lib/fidius/action.rb
@@ -2,8 +2,14 @@ module FIDIUS
   module Action
     include FIDIUS::Loudness
     
-    autoload :Scan, 'fidius/action/scan'
+    autoload :Scan,    'fidius/action/scan'
     autoload :Exploit, 'fidius/action/exploit'
-        
+    autoload :Msf,     'fidius/action/msf'
+    
+    def msf_framework
+      FIDIUS::Action::Msf.instance.framework
+    end
+    module_function :msf_framework
+    
   end # module Action
 end # module FIDIUS
