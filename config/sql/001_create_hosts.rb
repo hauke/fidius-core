e07e40dc63a0e041988758a4b333e372f215fd1b
Hauke Mehrtens <hauke@hauke-m.de>
2011-03-26 14:16:25 +0100

    reformat migrations

--- config/sql/001_create_hosts.rb
+++ config/sql/001_create_hosts.rb
@@ -1,18 +1,18 @@
 class CreateHosts < ActiveRecord::Migration
-	def self.up
-		create_table :hosts do |t|
-			t.string :name
+  def self.up
+    create_table :hosts do |t|
+      t.string :name
       t.string :ip
       t.integer :rating
-      t.boolean :exploited
+      t.boolean :exploited, :default => 0
       t.string :os_name
       t.string :os_sp
       t.integer :pivot_host_id # TODO: this is obsolet??
       t.integer :reachable_through_host_id
-		end
-	end
-	
-	def self.down
-		drop_table :hosts
-	end
+    end
+  end
+
+  def self.down
+    drop_table :hosts
+  end
 end
--- config/sql/002_create_services.rb
+++ config/sql/002_create_services.rb
@@ -1,15 +1,15 @@
 class CreateServices < ActiveRecord::Migration
-	def self.up
-		create_table :services do |t|
-			t.string :name
-			t.string :port
-			t.string :proto
+  def self.up
+    create_table :services do |t|
+      t.string :name
+      t.string :port
+      t.string :proto
       t.string :info
-			t.integer :host_id
-		end
-	end
-	
-	def self.down
-		drop_table :services
-	end
+      t.integer :host_id
+    end
+  end
+
+  def self.down
+    drop_table :services
+  end
 end
