94a4990464aba8f6adcd54a3fc2c34dda105e9bc
LarsMeyer <lmeyer@tzi.de>
2011-04-15 16:05:58 +0200

    preparing for test env ...

--- test/test_fidius.rb
+++ test/test_fidius.rb
@@ -1,10 +1,14 @@
+require 'simplecov'
+  SimpleCov.start
 require 'fidius'
 require 'test/unit'
+
 
 module FIDIUS
   class Test < Test::Unit::TestCase
     def setup
       FIDIUS.connect_db
+      #FIDIUS::Action::Msf.instance.start
     end
     
     def teardown
