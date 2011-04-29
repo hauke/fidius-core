46929002890ba1a983bd2efa413d355bc655baa5
Hauke Mehrtens <hauke@hauke-m.de>
2011-04-29 14:25:21 +0200

    tests: extend msf session mock

--- test/mock/msf.rb
+++ test/mock/msf.rb
@@ -83,25 +83,109 @@ module FIDIUS
 
     end #class ModuleSetMock
 
-    class SessionMock
+    module Session
+      class SessionMock
 
-      def initialize name, exploit_name
-        @exploit_name = exploit_name
-        @name = name
-      end
+        def initialize name, exploit_name
+          @exploit_name = exploit_name
+          @name = name
+          @net = NetMock.new
+          @sys = SysMock.new
+        end
 
-      def name
-        @name
-      end
+        def name
+          @name
+        end
 
-      def via_exploit
-        @exploit_name
-      end
+        def via_exploit
+          @exploit_name
+        end
 
-      def via_payload
-        "payload/test"
-      end
+        def via_payload
+          "payload/test"
+        end
 
-    end #class ModuleSetMock
+        def load_stdapi
+          #Nothing to do
+        end
+
+        def load_priv
+          #Nothing to do
+        end
+
+        def net
+          @net
+        end
+
+        def sys
+          @sys
+        end
+
+      end #class ModuleSetMock
+
+      class ConfigMock
+
+        def initialize
+          @ipAddresses = [InterfaceMock.new("127.0.0.1"), InterfaceMock.new("192.168.56.100")]
+        end
+
+        def each_interface(&block)
+          @ipAddresses.each(&block)
+        end
+
+      end # class ConfigMock
+
+      class NetMock
+
+        def initialize
+          @config = ConfigMock.new
+        end
+
+        def config
+          @config
+        end
+
+      end # class NetMock
+
+      class InterfaceMock
+
+        def initialize ip
+          @ip = ip
+        end
+
+        def ip
+          @ip
+        end
+
+        def netmask
+          "255.255.255.0"
+        end
+
+        def mac_addr
+          "00:11:22:33:44:55"
+        end
+
+      end # class InterfaceMock
+
+      class SysMock
+
+        def initialize
+          @process = ProcessMock.new
+        end
+
+        def process
+          @process
+        end
+
+      end # class SysMock
+
+      class ProcessMock
+
+        def get_processes
+          [{"name" => "explorer.exe"}]
+        end
+
+      end # module Session
+    end # class ProcessMock
   end # module MsfMock
 end # module FIDIUS
