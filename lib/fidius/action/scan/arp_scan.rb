896abefbbb5c9a4eba5cca2aedb2a4a8e54863a7
Hauke Mehrtens <hauke@hauke-m.de>
2011-05-13 13:07:38 +0200

    msf: store the rangewalker instance in the server so that the GC does not remove it.

--- lib/fidius/action/scan/arp_scan.rb
+++ lib/fidius/action/scan/arp_scan.rb
@@ -28,33 +28,38 @@ module FIDIUS
 
         def execute_msf session
           puts("ARP Scanning #{@target}")
-          ws = session.railgun.ws2_32
-          iphlp = session.railgun.iphlpapi
-          found = []
-          ipadd = FIDIUS::Action::Msf.instance.create_range_walker @target
-          while (ip_text = ipadd.next_ip)
-            puts "scan IP #{ip_text}"
-            h = ws.inet_addr(ip_text)
-            ip = h["return"]
-            h = iphlp.SendARP(ip,0,6,6)
-            if h["return"] == session.railgun.const("NO_ERROR")
-              mac = h["pMacAddr"]
+          ipadd = nil
+          begin
+            ws = session.railgun.ws2_32
+            iphlp = session.railgun.iphlpapi
+            found = []
+            ipadd = FIDIUS::Action::Msf.instance.create_range_walker @target
+            while (ip_text = ipadd.next_ip)
+              puts "scan IP #{ip_text}"
+              h = ws.inet_addr(ip_text)
+              ip = h["return"]
+              h = iphlp.SendARP(ip,0,6,6)
+              if h["return"] == session.railgun.const("NO_ERROR")
+                mac = h["pMacAddr"]
 
-              # XXX: in Ruby, we would do
-              #   mac.map{|m| m.ord.to_s 16 }.join ':'
-              # and not
-              mac_str = mac[0].ord.to_s(16) + ":" +
-                  mac[1].ord.to_s(16) + ":" +
-                  mac[2].ord.to_s(16) + ":" +
-                  mac[3].ord.to_s(16) + ":" +
-                  mac[4].ord.to_s(16) + ":" +
-                  mac[5].ord.to_s(16)
-              puts "IP: #{ip_text} MAC #{mac_str}"
-              found << "#{ip_text}"
-              #cmd_tcp_scanner(:rhost => ip_text, :ports => '20-25,80,120-140,440-450')
+                # XXX: in Ruby, we would do
+                #   mac.map{|m| m.ord.to_s 16 }.join ':'
+                # and not
+                mac_str = mac[0].ord.to_s(16) + ":" +
+                    mac[1].ord.to_s(16) + ":" +
+                    mac[2].ord.to_s(16) + ":" +
+                    mac[3].ord.to_s(16) + ":" +
+                    mac[4].ord.to_s(16) + ":" +
+                    mac[5].ord.to_s(16)
+                puts "IP: #{ip_text} MAC #{mac_str}"
+                found << "#{ip_text}"
+                #cmd_tcp_scanner(:rhost => ip_text, :ports => '20-25,80,120-140,440-450')
+              end
             end
+            return found
+          ensure
+            FIDIUS::Action::Msf.instance.destroy_range_walker ipadd
           end
-          return found
         end
 
       end # class ArpScan
