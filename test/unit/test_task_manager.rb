d2306975644cda20b632bc3d949c1770fcc9a316
Hauke Mehrtens <hauke@hauke-m.de>
2011-06-10 10:38:18 +0200

    test: make tests faster.
    just do not wait so long

--- test/unit/test_task_manager.rb
+++ test/unit/test_task_manager.rb
@@ -6,10 +6,10 @@ class TestTaskManager < FIDIUS::Test
   def test_simple_creation
     assert_equal 0, FIDIUS::Server::TaskManager.active_task_count
     t = FIDIUS::Server::TaskManager.new_task "Scan something" do
-      sleep 10
+      sleep 1
     end
     assert_equal 1, FIDIUS::Server::TaskManager.active_task_count
-    sleep 20
+    sleep 2
     assert FIDIUS::Server::TaskManager.completed?(t)
     FIDIUS::Server::TaskManager.remove_finished_tasks
     assert_equal 0, FIDIUS::Server::TaskManager.active_task_count
@@ -19,9 +19,9 @@ class TestTaskManager < FIDIUS::Test
   def test_kill_task
     assert_equal 0, FIDIUS::Server::TaskManager.active_task_count
     t = FIDIUS::Server::TaskManager.new_task "Scan something" do
-      sleep 10
+      sleep 1
     end
-    sleep 3
+    sleep 0.3
     assert_equal 1, FIDIUS::Server::TaskManager.active_task_count
     FIDIUS::Server::TaskManager.kill_task(t)
 
@@ -33,17 +33,17 @@ class TestTaskManager < FIDIUS::Test
   def test_start_multiple
     assert_equal 0, FIDIUS::Server::TaskManager.active_task_count
     t = FIDIUS::Server::TaskManager.new_task "Scan something" do
-      sleep 10
+      sleep 1
     end
     t = FIDIUS::Server::TaskManager.new_task "Scan something" do
-      sleep 10
+      sleep 1
     end
     t = FIDIUS::Server::TaskManager.new_task "Scan something" do
-      sleep 10
+      sleep 1
     end
     assert_equal 3, FIDIUS::Server::TaskManager.active_task_count    
     assert_equal 3, FIDIUS::Task.count(:all)
-    sleep 15    
+    sleep 1.5    
     assert_equal 0, FIDIUS::Server::TaskManager.active_task_count    
     assert_equal 3, FIDIUS::Task.count(:all)
   end
@@ -51,11 +51,11 @@ class TestTaskManager < FIDIUS::Test
   def test_set_progress
     t = FIDIUS::Server::TaskManager.new_task "Scan something" do |task|
       task.update_progress(10)
-      sleep 10
+      sleep 1
     end
-    sleep 5
+    sleep 0.5
     assert_equal 10, t.progress
-    sleep 10  
+    sleep 1  
     assert_equal 0, FIDIUS::Server::TaskManager.active_task_count    
     FIDIUS::Server::TaskManager.remove_finished_tasks
     assert_equal 0, FIDIUS::Task.count(:all)
