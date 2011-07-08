5c30d9b5c5bd6fa1bcb140c7bfa691994bb68e7c
Hauke Mehrtens <hauke@hauke-m.de>
2011-07-08 12:16:20 +0200

    add methode to reset agent

--- lib/fidius/server/handlers/decision.rb
+++ lib/fidius/server/handlers/decision.rb
@@ -2,11 +2,16 @@ module FIDIUS
   module Server
     module Handler
       class Decision < FIDIUS::Server::Handler::Base
-        
+
         def next_host
           result = FIDIUS::MachineLearning::AgendManager.instance.agent.next
           return result.id if result
           return -1
+        end
+
+        def reset_agent
+          FIDIUS::MachineLearning::AgendManager.instance.reset
+          ""
         end
 
       end
