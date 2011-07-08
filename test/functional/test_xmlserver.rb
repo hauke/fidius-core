ec97bd3fa4dab6487abfb9be1e6305063f33fda3
Hauke Mehrtens <hauke@hauke-m.de>
2011-07-08 09:24:57 +0200

    add test to call the ki from xml server

--- test/functional/test_xmlserver.rb
+++ test/functional/test_xmlserver.rb
@@ -43,4 +43,23 @@ class XMLServerTest < FIDIUS::Test
     assert_equal "Windows", h.os_name
     assert_equal h.interfaces[0].services.size, 7
   end
+
+  def test_ask_agent
+    xmlrpc = FIDIUS::Server::RPC.new
+    result = xmlrpc.call("decision.next_host")
+    assert_equal "<?xml version=\"1.0\" ?><methodResponse><params><param><value><i4>-1</i4></value></param></params></methodResponse>\n", result
+
+    result = xmlrpc.call("action.scan","192.168.56.0/24")
+    assert_equal "<?xml version=\"1.0\" ?><methodResponse><params><param><value><string>ok</string></value></param></params></methodResponse>\n", result
+
+    h = FIDIUS::Asset::Host.find_by_ip "192.168.56.101"
+    assert !h
+    sleep 4
+    h = FIDIUS::Asset::Host.find_by_ip "192.168.56.101"
+    assert h
+    result = xmlrpc.call("decision.next_host")
+    interface = result.scan(/<i4>(.*)<\/i4>/).flatten[0]
+    assert_equal 7, FIDIUS::Asset::Interface.find_by_id(interface).services.size
+    assert_equal "192.168.56.101", FIDIUS::Asset::Interface.find_by_id(interface).ip
+  end
 end
